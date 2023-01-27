4289fc8afff9a450bdbb2724edd4f7f0e045be2e
victor-cp <vcolquichaguap@uni.pe>
2023-01-27 13:49:48 -0500

    First commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        color1: "#056564",
+        color2: "#1fa2aa",
+        color3: "7ad4dd",
+        color4: "4fba73",
+        color5: "0a572f",
+        color6: "062715",
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
